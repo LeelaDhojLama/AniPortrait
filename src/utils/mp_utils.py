6f7307c57706a4e8718dee48222a5da81a93a6d5
Leela Dhoj Lama <ldlama@Leelas-MacBook-Pro.local>
2024-04-23 22:14:53 +0800

    replaced media pipe landmark detection with DECA with basic confit

--- src/utils/mp_utils.py
+++ src/utils/mp_utils.py
@@ -1,95 +1,56 @@
-import os
-import numpy as np
+from typing import Any
 import cv2
-import time
+import torch
 from tqdm import tqdm
-import multiprocessing
-import glob
+import numpy as np
 
-import mediapipe as mp
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from . import face_landmark
 
-CUR_DIR = os.path.dirname(__file__)
-
+from src.DECA.decalib.deca import DECA
+from src.DECA.decalib.datasets import datasets 
+from src.DECA.decalib.utils import util
+from src.DECA.decalib.utils.config import cfg as deca_cfg
+from src.DECA.decalib.utils.tensor_cropper import transform_points
 
 class LMKExtractor():
-    def __init__(self, FPS=25):
-        # Create an FaceLandmarker object.
-        self.mode = mp.tasks.vision.FaceDetectorOptions.running_mode.IMAGE
-        base_options = python.BaseOptions(model_asset_path=os.path.join(CUR_DIR, 'mp_models/face_landmarker_v2_with_blendshapes.task'))
-        base_options.delegate = mp.tasks.BaseOptions.Delegate.CPU
-        options = vision.FaceLandmarkerOptions(base_options=base_options,
-                                            running_mode=self.mode,
-                                            output_face_blendshapes=True,
-                                            output_facial_transformation_matrixes=True,
-                                            num_faces=1)
-        self.detector = face_landmark.FaceLandmarker.create_from_options(options)
-        self.last_ts = 0
-        self.frame_ms = int(1000 / FPS)
 
-        det_base_options = python.BaseOptions(model_asset_path=os.path.join(CUR_DIR, 'mp_models/blaze_face_short_range.tflite'))
-        det_options = vision.FaceDetectorOptions(base_options=det_base_options)
-        self.det_detector = vision.FaceDetector.create_from_options(det_options)
-                
+	def __init__(self) -> None:
+		pass
 
-    def __call__(self, img):
-        frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-        t0 = time.time()
-        if self.mode == mp.tasks.vision.FaceDetectorOptions.running_mode.VIDEO:
-            det_result = self.det_detector.detect(image)
-            if len(det_result.detections) != 1:
-                return None
-            self.last_ts += self.frame_ms
-            try:
-                detection_result, mesh3d = self.detector.detect_for_video(image, timestamp_ms=self.last_ts)
-            except:
-                return None
-        elif self.mode == mp.tasks.vision.FaceDetectorOptions.running_mode.IMAGE:
-            # det_result = self.det_detector.detect(image)
+	def __call__(open_cv_image) -> Any:
+		# load test images 
+		testdata = datasets.TestData(open_cv_image, iscrop=True, face_detector='fan', sample_step=10)
 
-            # if len(det_result.detections) != 1:
-            #     return None
-            try:
-                detection_result, mesh3d = self.detector.detect(image)
-            except:
-                return None
-            
-        
-        bs_list = detection_result.face_blendshapes
-        if len(bs_list) == 1:
-            bs = bs_list[0]
-            bs_values = []
-            for index in range(len(bs)):
-                bs_values.append(bs[index].score)
-            bs_values = bs_values[1:] # remove neutral
-            trans_mat = detection_result.facial_transformation_matrixes[0]
-            face_landmarks_list = detection_result.face_landmarks
-            face_landmarks = face_landmarks_list[0]
-            lmks = []
-            for index in range(len(face_landmarks)):
-                x = face_landmarks[index].x
-                y = face_landmarks[index].y
-                z = face_landmarks[index].z
-                lmks.append([x, y, z])
-            lmks = np.array(lmks)
-            
-            lmks3d = np.array(mesh3d.vertex_buffer)
-            lmks3d = lmks3d.reshape(-1, 5)[:, :3]
-            mp_tris = np.array(mesh3d.index_buffer).reshape(-1, 3) + 1
+		# run DECA
+		deca_cfg.model.use_tex = False
+		deca_cfg.rasterizer_type = 'pytorch3d'
+		deca_cfg.model.extract_tex = True
+		deca = DECA(config = deca_cfg, device='cuda')
+		# for i in range(len(testdata)):
+		for i in tqdm(range(len(testdata))):
+			name = testdata[i]['imagename']
+			images = testdata[i]['image'].to('cuda')[None,...]
+			with torch.no_grad():
+				codedict = deca.encode(images)
+				opdict, visdict = deca.decode(codedict) #tensor
+				#render_org in default
+				tform = testdata[i]['tform'][None, ...]
+				tform = torch.inverse(tform).transpose(1,2).to('cuda')
+				original_image = testdata[i]['original_image'][None, ...].to('cuda')
+				_, orig_visdict = deca.decode(codedict, render_orig=True, original_image=original_image, tform=tform)    
+				orig_visdict['inputs'] = original_image 
+				landmark_51 = opdict['landmarks3d_world'][:, 17:]
+				landmark_7 = landmark_51[:,[19, 22, 25, 28, 16, 31, 37]]
+				landmark_7 = landmark_7.cpu().numpy() 
 
-            return {
-                "lmks": lmks,
-                'lmks3d': lmks3d,
-                "trans_mat": trans_mat,
-                'faces': mp_tris,
-                "bs": bs_values
-            }
-        else:
-            # print('multiple faces in the image: {}'.format(img_path))
-            return None
-        
+				lmks = np.array(opdict['landmarks3d_world'])
+				
+				lmks3d = np.array(opdict['verts'])
+				lmks3d = lmks3d.reshape(-1, 5)[:, :3]
+				mp_tris = np.array(opdict['verts'].index).reshape(-1, 3) + 1
+
+				return {
+					"lmks": lmks,
+					'lmks3d': lmks3d,
+					"trans_mat": opdict['trans_verts'],
+					'faces': mp_tris,
+				}  
